ee235d6a61c581cf35dc95890b5f7273d32be118
ronitsrma78 <85008715+ronitsrma78@users.noreply.github.com>
2022-01-13 18:10:03 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const movies = [
+  {
+    id: 1001,
+    title: "The Train",
+    genre: "thriller and drama"
+  },
+  {
+    id: 1002,
+    title: "The Train",
+    genre: "thriller and drama"
+  },
+  {
+    id: 1003,
+    title: "The Hulk",
+    genre: "Action and Drama"
+  },
+  {
+    id: 1004,
+    title: "Hungama",
+    genre: "Comedy and drama"
+  },
+  {
+    id: 1005,
+    title: "Holiday",
+    genre: "thriller and Action"
+  }
+];
