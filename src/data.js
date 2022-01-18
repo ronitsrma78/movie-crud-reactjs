1e2ad53deab07f6b09148a1d8c65aa19f1f9f87e
ronitsrma78 <85008715+ronitsrma78@users.noreply.github.com>
2022-01-18 10:41:59 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export const data = [
+  {
+    title: "The New Book",
+    content: ` Culpa occaecat sint nostrud ex. Duis excepteur cillum deserunt est excepteur adipisicing consectetur aliquip enim est Lorem anim veniam.
+    Veniam enim occaecat nulla anim aute deserunt Lorem pariatur. Aliquip est voluptate veniam culpa officia.Do est eu deserunt fugiat dolore 
+     exercitation amet non elit ipsum cillum adipisicing non.`,
+    author: "Mr. John"
+  },
+  {
+    title: "Summer Vaccation ",
+    content: ` Culpa occaecat sint nostrud ex. Duis excepteur cillum deserunt est excepteur adipisicing consectetur aliquip enim est Lorem anim veniam.`,
+    author: "Lammy Silva"
+  },
+  {
+    title: "Rich and Poor People",
+    content: ` Culpa occaecat sint nostrud ex. Duis excepteur cillum deserunt est excepteur adipisicing consectetur aliquip enim est Lorem anim veniam.
+    Veniam enim occaecat nulla anim aute deserunt Lorem pariatur. Aliquip est voluptate veniam culpa officia.Do est eu deserunt fugiat dolore 
+     exercitation amet non elit ipsum cillum adipisicing non.`,
+    author: "Mark Das"
+  }
+];
