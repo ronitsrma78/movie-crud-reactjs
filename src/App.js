89ac496bab07571e3d400a84c3aa42c8d59adfdc
ronitsrma78 <85008715+ronitsrma78@users.noreply.github.com>
2022-01-29 17:03:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@
 import axios from 'axios';
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import ChildA from './ChildA';
 
 export default class App extends Component {
 
