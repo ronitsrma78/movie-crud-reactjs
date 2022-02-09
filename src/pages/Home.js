1773ba6ae2515451e90a67965f21d8b9c2accfe6
ronitsrma78 <85008715+ronitsrma78@users.noreply.github.com>
2022-02-09 18:20:56 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return <h2>Home </h2>;
+  }
+}
+
+export default Home;
+
+
+
+/**
+   Assignment
+   step-1 set context into this class context.
+   step-2 iterate this.context.data.users .
+*/
