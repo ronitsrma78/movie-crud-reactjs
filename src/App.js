a8ccebfec71086f2baa632b59da667b597ae22bd
ronitsrma78 <85008715+ronitsrma78@users.noreply.github.com>
2022-02-21 00:11:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Component } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NAvbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { context } from "./context";
+import Signup from "./pages/Signup";
 
 class App extends Component {
   constructor() {
@@ -12,15 +13,15 @@ class App extends Component {
 
   render() {
     //console.log(this.context.data.current_user);
-    const { current_user } = this.context.data;
+    const { current_user } = this.context;
     console.log(current_user);
 
     return (
       <BrowserRouter>
-        <NAvbar />
         <Routes>
           <Route path="/home" element={current_user ? <Home /> : <Login />} />
           <Route path="/login" element={current_user ? <Home /> : <Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     );
